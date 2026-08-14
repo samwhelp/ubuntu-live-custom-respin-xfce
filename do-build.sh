2730098eb9b15c7dcdcbca8de65a8a2fcf33c84c
samwhelp <samwhelp@users.noreply.github.com>
2026-08-15 04:21:21 +0800

    fix

--- do-build.sh
+++ do-build.sh
@@ -1655,9 +1655,9 @@ function model_do_fulfill_scripts () {
 	extend_hook_main_script_run
 
 
-	#base_dconf_package_install
-	#base_dconf_db_update
-	#base_gsettings_schema_compile
+	base_dconf_package_install
+	base_dconf_db_update
+	base_gsettings_schema_compile
 
 
 	kernel_initramfs_update
